3948c8649ae4d452497a61ccac92e42ebeeb6e3c
berkyaglioglu <berkyaglioglu@sabanciuniv.edu>
2019-07-24 17:43:40 +0300

    gender saved

--- users/managers.py
+++ users/managers.py
@@ -3,13 +3,13 @@ from django.contrib.auth.models import BaseUserManager
 
 class Manager(BaseUserManager):
     def create_user(self, email, first_name, last_name, password, gender):
-        user = self.model(email=email, first_name=first_name, last_name=last_name)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, gender=gender)
         user.set_password(password)
         user.save()
         return user
 
     def create_superuser(self, email, first_name, last_name, password, gender):
-        user = self.model(email=email, first_name=first_name, last_name=last_name, is_superuser=True, is_staff=True)
+        user = self.model(email=email, first_name=first_name, last_name=last_name, is_superuser=True, is_staff=True, gender=gender)
         user.set_password(password)
         user.save()
         return user
