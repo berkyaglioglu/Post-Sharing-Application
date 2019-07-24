397a31d45c7ce5ad2371d02884cead86bdd51847
berkyaglioglu <berkyaglioglu@sabanciuniv.edu>
2019-07-24 17:20:27 +0300

    fields added

--- users/managers.py
+++ users/managers.py
@@ -2,13 +2,13 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class Manager(BaseUserManager):
-    def create_user(self, email, first_name, last_name, password):
+    def create_user(self, email, first_name, last_name, password, gender):
         user = self.model(email=email, first_name=first_name, last_name=last_name)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, email, first_name, last_name, password):
+    def create_superuser(self, email, first_name, last_name, password, gender):
         user = self.model(email=email, first_name=first_name, last_name=last_name, is_superuser=True, is_staff=True)
         user.set_password(password)
         user.save()
