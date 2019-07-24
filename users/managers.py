55fec63a12004666812e530ff311b8139e1f9c40
berkyaglioglu <berkyaglioglu@sabanciuniv.edu>
2019-07-24 12:41:16 +0300

    AUTH_USER_MODEL is updated, superuser is created

---
+++ users/managers.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class Manager(BaseUserManager):
+    def create_user(self, email, first_name, last_name, password):
+        user = self.model(email=email, first_name=first_name, last_name=last_name)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_superuser(self, email, first_name, last_name, password):
+        user = self.create_user(email=email, first_name=first_name, last_name=last_name, password=password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save()
+        return user
+
--- users/models.py
+++ users/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import UserManager
+from .managers import Manager
 # Create your models here.
+
 
 class User(AbstractBaseUser):
     email = models.EmailField(
@@ -11,11 +14,21 @@ class User(AbstractBaseUser):
 
     first_name = models.TextField(max_length=50, blank=False, verbose_name='first name')
     last_name = models.TextField(max_length=50, blank=False, verbose_name='last name')
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    objects = Manager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['email', 'first_name', 'last_name']
+
+    REQUIRED_FIELDS = ['first_name', 'last_name']
 
     def __str__(self):
         return self.email
+
+    def has_perm(self, perm, obj=None):
+        return self.is_superuser
+
+    def has_module_perms(self, app_label):
+        return self.is_superuser
 
 
