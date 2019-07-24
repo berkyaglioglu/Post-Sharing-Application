b8cdd095e82e611157cef744c2e3630e209feda9
berkyaglioglu <berkyaglioglu@sabanciuniv.edu>
2019-07-24 15:38:10 +0300

    user fixed

--- users/models.py
+++ users/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
-from django.contrib.auth.models import UserManager
+from django.contrib.auth.models import PermissionsMixin
 from .managers import Manager
 # Create your models here.
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(
         verbose_name='email address',
         max_length=255,
@@ -15,7 +15,6 @@ class User(AbstractBaseUser):
     first_name = models.TextField(max_length=50, blank=False, verbose_name='first name')
     last_name = models.TextField(max_length=50, blank=False, verbose_name='last name')
     is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
     objects = Manager()
 
     USERNAME_FIELD = 'email'
@@ -24,11 +23,5 @@ class User(AbstractBaseUser):
 
     def __str__(self):
         return self.email
-
-    def has_perm(self, perm, obj=None):
-        return self.is_superuser
-
-    def has_module_perms(self, app_label):
-        return self.is_superuser
 
 
