a5d05d5e0cbf8b6de5d177c8e2b428074c3f2b4f
berkyaglioglu <berkyaglioglu@sabanciuniv.edu>
2019-07-25 13:02:54 +0300

    user changed

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from .forms import Register, LoginForm
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
 
+
 def register(request):
     if request.user.is_authenticated:
         return HttpResponseRedirect(reverse('posts:post_list'))
@@ -10,14 +11,11 @@ def register(request):
     form = Register(request.POST or None)
     if form.is_valid():
         user = form.save(commit=False)
-        username = form.cleaned_data['username']
         password = form.cleaned_data['password']
         user.set_password(password)
         user.save()
-        is_authenticated = authenticate(username=username, password=password)
-        if is_authenticated:
-            login(request, user)
-            return HttpResponseRedirect(reverse('posts:post_list'))
+        login(request, user)
+        return HttpResponseRedirect(reverse('posts:post_list'))
 
     return render(request, 'users/register.html', context={'form': form})
 
@@ -28,9 +26,9 @@ def user_login(request):
 
     form = LoginForm(request.POST or None)
     if form.is_valid():
-        username = form.cleaned_data['username']
+        email = form.cleaned_data['email']
         password = form.cleaned_data['password']
-        user = authenticate(username=username, password=password)
+        user = authenticate(email=email, password=password)
         if user:
             login(request, user)
             return HttpResponseRedirect(reverse('posts:post_list'))
